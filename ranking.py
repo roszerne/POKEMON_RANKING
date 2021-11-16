3feb1d1c131b8f33138f80d906b736545993c602
Ernest Roszak <74867891+roszerne@users.noreply.github.com>
2021-11-16 22:20:35 +0100

    Dodanie komentarzy

--- ranking.py
+++ ranking.py
@@ -6,13 +6,14 @@ from Pokemon import Pokemon
 class Ranking:
     def __init__(self, pokemons, scale):
         self.pokemons = pokemons
-        self.scale = scale
-        self.N = len(pokemons)
-        self.criterions = 4
-        self.C = np.ones((self.criterions, self.N, self.N), dtype='double')
-        self.priorities = np.zeros((self.N, self.criterions), dtype='double')
-        self.priority_vector = np.zeros((self.criterions, 1), dtype='double')
+        self.scale = scale # 2nd level PC matrix
+        self.N = len(pokemons) # numbers of alternatives
+        self.criterions = 4 # number of criterions
+        self.C = np.ones((self.criterions, self.N, self.N), dtype='double') # Pairwise comparison (PC) matrix
+        self.priorities = np.zeros((self.N, self.criterions), dtype='double') # priority vectors of 1st level PC matrices
+        self.priority_vector = np.zeros((self.criterions, 1), dtype='double') # priority vector derived from 2nd level PC matrix
 
+    # Creating PC matrices
     def createCriterion(self):
         for i in range(self.criterions):
             for j in range(0, self.N):
@@ -20,37 +21,39 @@ class Ranking:
                     self.C[i, j, k] = self.pokemons[j].crit[i] / self.pokemons[k].crit[i]
                     self.C[i, k, j] = 1 / self.C[i, j, k]
 
+    # Calculating eigenVectors of PC matrices
     def eigenValues(self):
         for i in range(self.criterions):
             A = self.C[i, :, :]
-            w, v = LA.eig(A)
+            w, v = LA.eig(A) # w - eigenvalues, v - eigenvectors
             w = abs(w)
             v = abs(v)
-            ind = np.argmax(w)  # znajdź największą wartość własną
+            ind = np.argmax(w)  # find eigenvector with bigges module
             self.priorities[:, i] = v[:, ind]
-        self.priorities = self.priorities / self.priorities.sum()  # znormalizuj wektory
+        self.priorities = self.priorities / self.priorities.sum()  # normalise the vectors
 
+    # Calculate priority vector of 2nd level PC Matrix
     def secondLevelMatrix(self):
-        w, v = LA.eig(self.scale)
+        w, v = LA.eig(self.scale) # w - eigenvalues, v - eigenvectors
         w = abs(w)
         v = abs(v)
-        ind = np.argmax(w)  # znajdź największą wartość własną
+        ind = np.argmax(w)  # find eigenvector with bigges module
         self.priority_vector = v[:, ind]
-        self.priority_vector = self.priority_vector / self.priority_vector.sum()  # znormalizuj wektory
+        self.priority_vector = self.priority_vector / self.priority_vector.sum()  # normalise the vectors
 
     def AHP(self):
-        # create matrices of criterias
+        # create PC matrices
         self.createCriterion()
         print('C: ', self.C)
-        # Oblicz wartości własne
+        # Calculate eigenvectors of PC matrices
         self.eigenValues()
         print('Priorities: ', self.priorities)
-        # second-level PC matrix
+        # eigenvector second-level PC matrix
         self.secondLevelMatrix()
-        print('C2: ', self.priority_vector)
-        # Final rank
+        print('eigenvector of C2: ', self.priority_vector)
+        # Final ranking vector
         final = np.dot(self.priorities, self.priority_vector)
-        final = final / final.sum()
+        final = final / final.sum() # normalise sum 
         print('Final: ', final)
         return final
 
